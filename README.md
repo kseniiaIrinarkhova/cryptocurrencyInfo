b7660c8b1adfde61b61103c971022fb211a47846
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-08 15:09:35 -0800

    added comments

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Cryptocurrency informational app
 React + Vite application, that shows usage of Routes in React. Information about currencies and prices was taken from [CoinAPI](https://www.coinapi.io/).
-    To get free API key use this [link](https://www.coinapi.io/get-free-api-key?product_id=market-data-api)
+- To get free API key use this [link](https://www.coinapi.io/get-free-api-key?product_id=market-data-api)
+- To run the application create `.env` based on `.env.example` and add API key information.
--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -6,43 +6,38 @@ import { useParams } from "react-router-dom";
 import axios from 'axios';
 
 export default function Price() {
-  const API_KEY= import.meta.env.VITE_API_KEY;
+  //take URL parameter
   const {symbol} = useParams();
-
-  const url = `https://rest.coinapi.io/v1/exchangerate/${symbol}/USD`
-
+  //creating url for rate request
+  const url = `${import.meta.env.VITE_API_URL}/${symbol}/USD`
+  //create a state - to show the information about rates
   const[coin, setCoin] = useState(null);
 
+  /**
+   * function that is used in useEffect
+   */
   async function getCoin(){
-    console.log(symbol)
-    axios.get(url, { headers: { 'X-CoinAPI-Key': API_KEY } })
+    //call the API
+    axios.get(url, { headers: { 'X-CoinAPI-Key': import.meta.env.VITE_API_KEY } })
     .then((res)=>{
-      
-      console.log(res.data)
+      //set state for new data
       setCoin(res.data)
     })
     .catch((err)=>{
       console.log({ error: err })
     })
     
-    // try {
-    //   const response = await axios.get(url, { headers: { 'X-CoinAPI-Key': API_KEY } })
-    //   const data = a
-    //   setCoin(response.data)
-    //   console.log(response)
-    // } catch (err) {
-    //   console.log({error: err})
-    // }
   }
 
   useEffect(()=>{
+    //try to get rate information
     getCoin();
   },[]);
 
-  // loaded function for when data is fetched.
+  // loaded function for when data is resieved.
   const loaded = () => {
     return (
-      <div>
+      <div className="price">
         <h1>
           {coin.asset_id_base}/{coin.asset_id_quote}
         </h1>
@@ -51,12 +46,10 @@ export default function Price() {
     );
   };
 
-  // Function for when data doesn't exist.
+  // Function for when data doesn't exist or loading.
   const loading = () => {
     return <h1>Loading...</h1>;
   };
-
-
 
   // If coin has data, run the loaded function; otherwise, run loading.
   return coin && coin.rate ? loaded() : loading();
