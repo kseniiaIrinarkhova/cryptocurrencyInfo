522ddf34e7477b21e3af6eb5b7640a4c80e6757a
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-08 15:13:38 -0800

    changed main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,9 @@
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>
+      <h1>Welcome to the Crypto prices app!</h1>
+      <h3>Choose currencies to see the prices in USD</h3>
+    </div>
   )
 }
